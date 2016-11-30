e886be92ae52b36e7431464c5b45270028476ec9
dan-nguyen <dtn601@gmail.com>
2016-11-30 00:04:00 -0600

    finished assignment

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,68 @@
 // API Docs at: 
 // https://developer.spotify.com/web-api/search-item/
+$(function(){
+
+	//console.log('spotify has loaded')
+
+	$('#search').on('submit',function(e){
+		e.preventDefault();
+
+		$('#results').empty();
 
 
-function searchByArtist(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
-}
+		var keyword = $('#search-keyword').val(),
+			type = $('#search-type').val(),
+			url = 'https://api.spotify.com/v1/search?q='+keyword+'&type='+type,
+			request = $.ajax({
+						url: url
+				});
+
+		if (type === 'artist'){
+
+		 	request.done(function(response){
+
+		 		var results = response.artists.items;
+		 		console.log(results);
+		 		for (var i=0, x=results.length; i<x; i++){
+		 			var list = results[i];}
+		 			console.log('results:', list);
+		 		
+		 			results.forEach(function(data) {
+        			$('#results').append("<li>" + data.name + "</li>")
+        			});
+		 		});
+
+		
+		} else { 
+		 	request.done(function(response){
+
+		 		var results = response.tracks.items;
+		 		console.log(results);
+		 		for (var i=0, x=results.length; i<x; i++){
+		 			var list = results[i];}
+		 			console.log('results:', list);
+		 		
+		 			results.forEach(function(data) {
+        			$('#results').append("<li>" + data.name + "</li>")
+        			});
+		 		});
+		  
+			}
+			  
+});
+});
 
 
-function searchByTrack(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
-}
+
+			// 		function searchByArtist(keyword) {
+ //		    var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+ 	//	  console.log('searchbyartist')			
+
+ 	//	  searchByArtist()
+ 	//	  console.log(url)
+
+
+	//	function searchByTrack(keyword) {
+	//	    var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
+	//		console.log('searchbytrack')
+	//	}
