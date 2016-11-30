0bd880d5441f9513a6dce3588d5b0fece4b4f8f3
Stephanie Workman <sworkman2014@gmail.com>
2016-11-30 09:42:39 -0600

    added some functionality

--- js/spotify.js
+++ js/spotify.js
@@ -1,5 +1,49 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/web-api/search-item/
+// API Docs at:
+// https://developer.spotify.com/web-api/search-item/
+$(function(){
+
+  $('#search').on('submit', function(e){
+      console.log('user submitted form');
+      e.preventDefault();
+
+        var keyword = $('#search-keyword').val();
+          console.log('search keyword', keyword);
+        // if ($('search-type').val() === 'artist'){
+          // function (searchByArtist(keyword){
+          var request = $.ajax({
+            url: 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist'
+
+          });
+        request.done(function(artistData) {
+          var artistResults = artistData.artists //then what???
+          console.log('Artist data', artistData)
+        });
+      // })
+      request.fail(function(jqXHR, textStatus, errorThrown) {
+        console.log('Error:', textStatus, errorThrown);
+      })
+  });
+});
+//======for track search====/////////
+        // if ($('search-type').val() === 'artist'){
+          // function (searchByArtist(keyword){
+  //         var request = $.ajax({
+  //           url: 'https://api.spotify.com/v1/search?q='+keyword+'&type=track'
+  //
+  //         });
+  //         request.done(function(trackData) {
+  //           var trackResults = trackData //then what???
+  //           console.log('Track data', trackResults)
+  //           // })
+  //         });
+  //
+  // });
+
+
+
+// ========provided code
 
 
 function searchByArtist(keyword) {
