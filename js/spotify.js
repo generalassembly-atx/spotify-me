065dff29d12e2f15ed99f7919307a8547c2dd966
Kyle Crichton <kylecrichton@gmail.com>
2016-11-29 21:57:46 -0600

    Tried my best to add search function

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,43 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/web-api/search-item/
+var userInput = $("#search-keyword");
+var form = $("#search");
+var results = $('#results');
 
 
-function searchByArtist(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
-}
+// function searchByArtist(keyword) {
+//   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+// }
+//
+//
+// function searchByTrack(keyword) {
+//   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
+// }
+form.on('submit', function(event){
+  event.preventDefault();
+
+  var searchType = $('#search-type').val();
+  var formInput = userInput.val();
+  $.ajax ({
+      url: 'https://api.spotify.com/v1/search?q=' + formInput + '&type=' + searchType,
+
+      success: function( response) {
+        console.log(response)
+        console.log(response.items)
+
+        plural = searchType + 's';
+        console.log(plural)
 
 
-function searchByTrack(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
-}
+        response[plural].items.forEach( function(item){
+          listItem = $("<li>"+ item.name  + "</li>");
+          console.log(listItem);
+
+          results.append(listItem);
+          console.log(results);
+          $('#totalResults').text(results.length);
+
+        })
+    }
+  })
+});
