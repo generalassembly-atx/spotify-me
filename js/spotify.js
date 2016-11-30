46e3f158d20bc5e0eaacaf7325f976c70463d19e
Kyle Crichton <kylecrichton@gmail.com>
2016-11-29 22:12:19 -0600

    fixed search function I believe

--- js/spotify.js
+++ js/spotify.js
@@ -5,14 +5,14 @@ var form = $("#search");
 var results = $('#results');
 
 
-// function searchByArtist(keyword) {
-//   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
-// }
-//
-//
-// function searchByTrack(keyword) {
-//   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
-// }
+function searchByArtist(keyword) {
+  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+}
+
+
+function searchByTrack(keyword) {
+  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
+}
 form.on('submit', function(event){
   event.preventDefault();
 
