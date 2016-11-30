66d9d984fd508f778a8e533c490d411423518c30
Stephanie Gage <stephig85@gmail.com>
2016-11-30 09:05:56 -0600

    unfinished.

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,29 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/web-api/search-item/
+console.log('spotify js working');
+
+$(document).ready(function (){
 
 
-function searchByArtist(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
-}
 
 
-function searchByTrack(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
-}
+  // allows user to search by artist
+  function searchByArtist(keyword) {
+    var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+    $('#search').on('submit', function(e){
+
+      $('#results').empty();
+    });
+  }
+
+  // allows user to search by keyword
+  function searchByTrack(keyword) {
+    var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
+
+        $('#search').on('submit', function(e){
+
+          $('#results').empty();
+        });
+  }
+
+}); // end of document ready
