0a95ce6733d308da0740ba3b95d75027812ab313
brittshroyer <britt.shroyer@gmail.com>
2016-11-29 23:34:35 -0600

    first

--- js/spotify.js
+++ js/spotify.js
@@ -1,12 +1,60 @@
-// API Docs at: 
+// API Docs at:
 // https://developer.spotify.com/web-api/search-item/
 
 
-function searchByArtist(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
-}
+// function searchByArtist(keyword) {
+//   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
+// }
 
 
-function searchByTrack(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
-}
+// function searchByTrack(keyword) {
+//   var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
+// }
+
+
+$(function(){
+
+  $('#submit').on('click', function(e){
+    // console.log("submit!");
+    e.preventDefault();
+
+    $('#results').empty();
+
+    var artistName = $('#search-keyword').val();
+
+
+    // console.log(artist);
+    var request = $.get('https://api.spotify.com/v1/search?q='+artistName+'&type=artist');
+
+    request.done(function(request){
+      var artistName = request;
+      console.log(artistName);
+
+      var y = artistName.items;
+      console.log(y);
+      // var searchResults = request.Search;
+      // console.log(artistName);
+
+
+    // request.done(function(artistName){
+    //   var searchResults = artistName.Search;
+    //
+    //   console.log(searchResults);
+    // });
+
+
+      for(var i=0; i<artistName.length; i++){
+        var artist = artistName[i];
+
+        var elements = ['<li>','<h2>'+ artist.name + '</h2>','</li>'].join('');
+
+        $('#results').append(elements);
+
+      }
+
+    });
+
+
+  });
+
+});
