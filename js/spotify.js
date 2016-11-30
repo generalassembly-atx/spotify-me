6525e5327dff0ba1ae7722e16b4e71be6e78c885
Stephanie Gage <stephig85@gmail.com>
2016-11-30 10:18:00 -0600

    another unfinished version... not sure which direction to go.

--- js/spotify.js
+++ js/spotify.js
@@ -4,26 +4,32 @@ console.log('spotify js working');
 
 $(document).ready(function (){
 
+  $('#search').on('submit', function(e){
+
+    $('#results').empty();
+  });
+
+  var request = $.ajax({
+        url: 'https://api.spotify.com/v1/search',
+        method: 'GET'
+      });
+
+  var artistName = $('.artistSearch').val();
+    console.log('Artist:', artistName);
+
+  var songName = $('.trackSearch').val();
+    console.log('Track:', songName);
 
 
 
   // allows user to search by artist
   function searchByArtist(keyword) {
     var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=artist';
-    $('#search').on('submit', function(e){
-
-      $('#results').empty();
-    });
   }
 
   // allows user to search by keyword
   function searchByTrack(keyword) {
     var url = 'https://api.spotify.com/v1/search?q='+keyword+'&type=track';
-
-        $('#search').on('submit', function(e){
-
-          $('#results').empty();
-        });
   }
 
 }); // end of document ready
