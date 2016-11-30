dfb9a138dba499848dbb140e09b77fca70633d31
vinh huynh <vvvhuynh@yahoo.com>
2016-11-30 11:38:20 -0600

    refactor code for DRY reasons

--- js/spotify.js
+++ js/spotify.js
@@ -1,56 +1,48 @@
 // API Docs at:
 // https://developer.spotify.com/web-api/search-item/
 
-
 $(document).ready(function() {
 
   $('#search').on('submit', function(event) {
+    //'submit' on forms will have a default behavior of clearing so we add event.preventDefault()
     event.preventDefault();
 
+    //clears the results div
     $('#results').empty();
 
+    //our search query
     var searchReq = $('#search-keyword').val();
 
     //checks to see which drop-down option is selected
-    if ($('#search-type option:selected').val() === "artist") {
-      searchByArtist(searchReq);
-    } else if ($('#search-type option:selected').val() === "track") {
-      searchByTrack(searchReq);
-    }
+    var searchType = $('#search-type').val();
 
-    //Search by Artist
-    function searchByArtist(artistReq) {
-      var requestArtist = $.ajax({
-        url: 'https://api.spotify.com/v1/search',
-        data: {
-          q: artistReq,
-          type: 'artist'
-        }
-      })
-      requestArtist.done(function(data) {
-        var artistArray = data.artists.items;
-        artistArray.forEach(function(val) {
-          $('#results').append('<li>' + val.name + '</li>');
-        })
-      })
-    }
-
-    //Search by Track
-    function searchByTrack(trackReq) {
-      var requestTrack = $.ajax({
-        url: 'https://api.spotify.com/v1/search',
-        data: {
-          q: trackReq,
-          type: 'track'
-        }
-      })
-      requestTrack.done(function(data) {
-        var trackArray = data.tracks.items;
-        trackArray.forEach(function(val) {
-          $('#results').append('<li>' + val.name + '</li>');
-        })
-      })
+    //calls the corresponding function based on 'searchType'
+    switch (searchType) {
+      case 'artist':
+        search(searchReq, 'artist', 'artists');
+        break;
+      case 'track':
+        search(searchReq, 'track', 'tracks');
+        break;
     }
   })
+
+  //Makes the AJAX request and iterates/displays the info
+  function search(searchReq, type, key) {
+    var request = $.ajax({
+      url: 'https://api.spotify.com/v1/search',
+      data: {
+        q: searchReq,
+        type: type
+      }
+    });
+    //on success, iterates & appends element to html
+    request.done(function(data) {
+      var resultsArray = data[key].items;
+      resultsArray.forEach(function(val) {
+        $('#results').append('<li>' + val.name + '</li>');
+      })
+    })
+  }
 
 })
